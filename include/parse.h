f806ac2095ecb177b64f7a38fb17cf30e5df1b42
Kevin Berry <soundmanberry@gmail.com>
2026-02-14 16:56:32 -0600

    Banish tabs in parse header file.

--- include/parse.h
+++ include/parse.h
@@ -4,16 +4,16 @@
 #define HEADER_MAGIC 0x4c4c4144
 
 struct dbheader_t {
-	unsigned int magic;
-	unsigned short version;
-	unsigned short count;
-	unsigned int filesize;
+    unsigned int magic;
+    unsigned short version;
+    unsigned short count;
+    unsigned int filesize;
 };
 
 struct employee_t {
-	char name[256];
-	char address[256];
-	unsigned int hours;
+    char name[256];
+    char address[256];
+    unsigned int hours;
 };
 
 int create_db_header(int fd, struct dbheader_t **headerOut);
